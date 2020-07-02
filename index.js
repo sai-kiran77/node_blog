6f315cad6d55ae441d54099e9d098f69b131411f
manchikantisaikiran <manchikantisaikiran777@gmail.com>
2020-07-02 23:22:03 +0530

    setup for heroku

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const express = require('express')
 const methodOverride = require('method-override')
+
 const articleRoutes = require('./routes/article')
-const app = express()
 const Article = require('./db/models/article')
-app.use(express.urlencoded({ extended: false }))
 require('./db/mongoose')
-app.set('view engine', 'ejs')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
 app.use('/article', articleRoutes)
 
-
+app.set('view engine', 'ejs')
 
 app.get('/', async (req, res) => {
     const articles = await Article.find().sort({ CreatedAt: 'desc' })
     res.render('articles/index', { articles })
 })
 
-app.listen(3000, () => {
-    console.log("server is running on port 3000");
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
 
 })
